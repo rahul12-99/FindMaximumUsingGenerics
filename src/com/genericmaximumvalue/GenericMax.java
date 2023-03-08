37c0615fc1306e71621c9b042308b3d5d1357870
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-09 03:50:51 +0530

    Added: Code for find maximum integer

--- src/com/genericmaximumvalue/GenericMax.java
+++ src/com/genericmaximumvalue/GenericMax.java
@@ -1,7 +1,20 @@
 package com.genericmaximumvalue;
 
 public class GenericMax {
+    static void testMax(Integer a, Integer b, Integer c) {
+        Integer maximum = 0;
+        if (a.compareTo(maximum) > 0) {
+            maximum = a;
+        } if (b.compareTo(maximum) > 0) {
+            maximum = b;
+        } if (c.compareTo(maximum) > 0) {
+            maximum = c;
+        }
+        System.out.println("Maximum is " + maximum);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to generics problem");
+        Integer a =50 ,b = 40, c = 30;
+        GenericMax.testMax(a,b,c);
     }
 }
